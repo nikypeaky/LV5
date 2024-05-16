1c8a378a0ce41374972ac482eca49f924c858027
nikol <nikol@NIKYPEAKY>
2024-05-16 15:45:51 +0200

    Add project files.

---
+++ lv05/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace lv05
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Product Product1 = new Product("laptop", 1500.99, 2.3);
+            Product Product3 = new Product("lamp",30.99, 1.5);
+            Product Product2 = new Product("stapler", 15.99, 0.2);
+            
+
+            Box box = new Box("office supplies");
+            box.Add(Product2);
+            box.Add(Product3);
+
+            Box BiggerBox = new Box("Package");
+            BiggerBox.Add(box);
+            BiggerBox.Add(Product1);
+
+            Console.WriteLine($"Total price: {BiggerBox.Price}");
+            Console.WriteLine($"Total weight: {BiggerBox.Weight}");
+            Console.WriteLine(BiggerBox.Description());
+        }
+    }
+}
